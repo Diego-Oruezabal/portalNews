18494beb36c8f3ee5be262d3fe9c7121d50d81d6
Diego Oruezabal <diegooruezabal@gmail.com>
2025-05-17 23:11:08 +0200

    Configuración inicial Angular e incorporación de plantilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FavoriteComponent } from "./components/favorite/favorite.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FavoriteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
