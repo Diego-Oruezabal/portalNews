2e3b1c29004fa2f12b5453b439b6922ccb8d3f36
Diego Oruezabal <diegooruezabal@gmail.com>
2025-05-18 21:15:30 +0200

    Estructura general de home, footer y favorite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FavoriteComponent } from "./components/favorite/favorite.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
