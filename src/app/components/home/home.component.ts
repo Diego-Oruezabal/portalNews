77c722530c6e80fd9d3ff61aa1c09a8dc024a7d0
Diego Oruezabal <diegooruezabal@gmail.com>
2025-05-19 17:19:17 +0200

    Mostrar información en la vista

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NewsService } from '../../services/news.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
